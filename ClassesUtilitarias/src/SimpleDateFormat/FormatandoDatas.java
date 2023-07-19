0e2789744ce5f2805cb44a7db70c93c6f7286387
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-19 15:16:54 -0300

    Update Formatando datas

--- ClassesUtilitarias/src/SimpleDateFormat/FormatandoDatas.java
+++ ClassesUtilitarias/src/SimpleDateFormat/FormatandoDatas.java
@@ -1,9 +1,22 @@
 package SimpleDateFormat;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class FormatandoDatas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Padrao -> formato da data
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy  HH:mm:ss a");
+		
+		Calendar data = new GregorianCalendar(2010, 2 , 20, 14, 32, 25);
+		
+		System.out.println(sdf.format(data.getTime()));
+		
+		
+		
+		
 
 	}
 
