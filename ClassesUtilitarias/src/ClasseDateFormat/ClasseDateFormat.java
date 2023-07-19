0b45d9c96cafea02762104948c5715a20cd6fece
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-19 15:47:40 -0300

    Formatação de Datas

--- ClassesUtilitarias/src/ClasseDateFormat/ClasseDateFormat.java
+++ ClassesUtilitarias/src/ClasseDateFormat/ClasseDateFormat.java
@@ -12,13 +12,21 @@ public class ClasseDateFormat {
 		System.out.println(hoje);
 		
 		//Definir formato de outro local
-		Locale.setDefault(new Locale("US"));
+		Locale.setDefault(new Locale("en", "US"));
 		//Formatar data de acordo com o locale
 		String hojeFormatado = DateFormat.getInstance().format(hoje);
 		System.out.println(hojeFormatado);
 		//GetInstance
 		hojeFormatado = DateFormat.getTimeInstance().format(hoje);
 		System.out.println(hojeFormatado);
+		
+		hojeFormatado = DateFormat.getTimeInstance(DateFormat.SHORT).format(hoje);
+		System.out.println(hojeFormatado);
+		
+		hojeFormatado = DateFormat.getTimeInstance(DateFormat.LONG).format(hoje);
+		System.out.println(hojeFormatado);
+		
+		
 	}
 
 }
