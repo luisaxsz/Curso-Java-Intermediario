42a89fd722c88cd06c79465bd3684a557b6b7997
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2023-07-20 15:09:06 -0300

    Jars

---
+++ InstalandoJars/src/UsandoJars/UsandoJars.java
@@ -0,0 +1,9 @@
+package UsandoJars;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class UsandoJars {
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("Jars"));
+	}
+}
